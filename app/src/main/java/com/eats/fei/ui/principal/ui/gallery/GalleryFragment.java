c65c0317913f87f95e3dcaabf919d3bed653e008
Jhair Sedeño <nemesist39@gmail.com>
2020-04-22 01:27:18 -0500

    Editar/Actualizar usuario implementado
    Signed-off-by: Jhair Sedeño <nemesist39@gmail.com>

--- app/src/main/java/com/eats/fei/ui/principal/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/eats/fei/ui/principal/ui/gallery/GalleryFragment.java
@@ -1,9 +1,11 @@
 package com.eats.fei.ui.principal.ui.gallery;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,6 +13,8 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.eats.fei.R;
+import com.eats.fei.ui.registrar.EditarActivity;
+import com.eats.fei.ui.registrar.RegistrarActivity;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -27,6 +31,7 @@ public class GalleryFragment extends Fragment {
     private GalleryViewModel galleryViewModel;
 
 
+
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         galleryViewModel = ViewModelProviders.of(this).get(GalleryViewModel.class);
         View root = inflater.inflate(R.layout.fragment_gallery, container, false);
@@ -34,6 +39,18 @@ public class GalleryFragment extends Fragment {
         final TextView nombre = root.findViewById (R.id.textView3);
         final TextView telefono = root.findViewById (R.id.textView14);
         final TextView correo = root.findViewById (R.id.textView13);
+       final Button button2 =  root.findViewById(R.id.button2);
+
+
+       /*Apartado para boton*/
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              startActivity(new Intent(GalleryFragment.this.getContext(), EditarActivity.class));
+            }
+
+        });
+        /*Fin del apartado*/
 
         dDatabase = FirebaseDatabase.getInstance();
         FirebaseUser user = firebaseAuth.getInstance ().getCurrentUser();
@@ -51,15 +68,27 @@ public class GalleryFragment extends Fragment {
                     nombre.setText("Nombre: " + nombre1);
                     telefono.setText("Teléfono: " + telefono1);
                     correo.setText("Correo: " + correo1);
+
+
                 }
+
+
+
+
+
             }
+
+
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
 
             }
+
+
         });
 
         return root;
+
 
     }
 
---
+++ app/src/main/java/com/eats/fei/ui/registrar/EditarActivity.java
@@ -0,0 +1,125 @@
+package com.eats.fei.ui.registrar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.eats.fei.R;
+import com.eats.fei.ui.principal.PrincipalActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class EditarActivity extends AppCompatActivity {
+
+    //Variables ocupadas
+    private EditText NombreE;
+    private EditText TelefonoE;
+    private EditText CorreoE;
+    private EditText ContrasenaE;
+    private Button btnCambios;
+
+
+    //VARIABLES PARA ALMACENAR LOS DATOS
+    private String nombrex = "";
+    private String telefonox = "";
+    private String correox = "";
+    private String contrasenax = "";
+
+
+    //Referencias
+    private DatabaseReference dDatabase;
+    private FirebaseAuth firebaseAuth;
+
+    //VARIABLES DE FIREBASE
+    /*FirebaseAuth mAuth; //AUTENTICACIÓN
+    DatabaseReference mDatabase;//USAR LA BASE DE DATOS REAL TIME*/
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_edit);
+
+        NombreE = (EditText) findViewById(R.id.txtNombre);
+        TelefonoE = (EditText) findViewById(R.id.txtTelefono);
+        CorreoE = (EditText) findViewById(R.id.txtCorreo);
+        ContrasenaE = (EditText) findViewById(R.id.txtContrasena);
+        btnCambios = (Button) findViewById(R.id.btnCambios);
+
+        ContrasenaE.setEnabled(false);
+        CorreoE.setEnabled(false);
+
+        /*mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+*/
+        dDatabase = FirebaseDatabase.getInstance().getReference(); //Aqui obtienes la referencia de la base de datos
+        FirebaseUser user = firebaseAuth.getInstance ().getCurrentUser();
+
+        /**/
+        //Referencia al nodo              aqui la referencia al usuario
+        dDatabase.child("Usuarios").child(user.getUid()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()){
+                    //Obtiene el valor de cada campo de un usuario
+                    String nombre1 = dataSnapshot.child("Nombre").getValue().toString();
+                    String telefono1 = dataSnapshot.child("Telefono").getValue().toString();
+                    String correo1 = dataSnapshot.child("Correo").getValue().toString();
+                    String contrasena1 = dataSnapshot.child("Contrasena").getValue().toString();
+
+                    //Asigna el valor de la variable al TextView correspondiente
+                    NombreE.setText(nombre1);
+                    TelefonoE.setText(telefono1);
+                    CorreoE.setText(correo1);
+                    ContrasenaE.setText(contrasena1);
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
+
+        /**/
+
+
+        btnCambios.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nombrex = NombreE.getText().toString();
+                telefonox = TelefonoE.getText().toString();
+                correox = CorreoE.getText().toString();
+                contrasenax = ContrasenaE.getText().toString();
+
+                Map<String, Object> map = new HashMap<>();
+                map.put("Telefono", telefonox);
+                map.put("Correo", correox);
+                map.put("Contrasena", contrasenax);
+                map.put("Nombre", nombrex);
+
+                dDatabase = FirebaseDatabase.getInstance().getReference(); //Aqui obtienes la referencia de la base de datos
+                FirebaseUser user = firebaseAuth.getInstance ().getCurrentUser();
+
+                dDatabase.child("Usuarios").child(user.getUid()).updateChildren(map);
+
+            }
+        });
+
+    }
+
+}
