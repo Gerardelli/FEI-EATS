4d19173d25b59dd838705e6149ef9ee53b74408e
Jhair Sedeño <nemesist39@gmail.com>
2020-05-14 20:37:04 -0500

    Apartado de agregar producto
    Signed-off-by: Jhair Sedeño <nemesist39@gmail.com>

--- app/src/main/java/com/eats/fei/ui/principal/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/com/eats/fei/ui/principal/ui/slideshow/SlideshowFragment.java
@@ -1,9 +1,11 @@
 package com.eats.fei.ui.principal.ui.slideshow;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,9 +15,16 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.eats.fei.R;
+import com.eats.fei.ui.principal.ui.gallery.GalleryFragment;
+import com.eats.fei.ui.registrar.EditarActivity;
+import com.eats.fei.ui.registrar.RegistrarProductoActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SlideshowFragment extends Fragment {
 
+    private FirebaseDatabase dDatabase;
+    private FirebaseAuth firebaseAuth = null;
     private SlideshowViewModel slideshowViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -23,6 +32,29 @@ public class SlideshowFragment extends Fragment {
         slideshowViewModel =
                 ViewModelProviders.of(this).get(SlideshowViewModel.class);
         View root = inflater.inflate(R.layout.fragment_slideshow, container, false);
+
+        final Button button3 = root.findViewById(R.id.button3);
+
+        /*Apartado para boton*/
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SlideshowFragment.this.getContext(), RegistrarProductoActivity.class));
+            }
+
+        });
+        /*Fin del apartado*/
+
+
+
+
+
+
+
+
+
+
+
         return root;
     }
 }
