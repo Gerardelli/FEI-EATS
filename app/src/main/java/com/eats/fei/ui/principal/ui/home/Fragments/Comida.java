c2e4c01ec9eee0e543fb4cd3b660bee079bed24e
Gerardelli Santiago Cruz <estivcap@gmail.com>
2020-05-21 17:20:25 -0500

    Se agregaron las tabs para la pantalla de unicio en donde se mostraran los productos por categoría, se agregó un archivo controlador que ayuda en ver que fragment se muestra dependiendo de cada tab seleccionado

---
+++ app/src/main/java/com/eats/fei/ui/principal/ui/home/Fragments/Comida.java
@@ -0,0 +1,29 @@
+package com.eats.fei.ui.principal.ui.home.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.eats.fei.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Comida extends Fragment {
+
+    public Comida() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_comida, container, false);
+    }
+}
--- app/src/main/java/com/eats/fei/ui/principal/ui/home/HomeFragment.java
+++ app/src/main/java/com/eats/fei/ui/principal/ui/home/HomeFragment.java
@@ -4,17 +4,19 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
 import com.eats.fei.R;
+import com.eats.fei.ui.principal.ui.home.Contoller.PagerController;
+import com.eats.fei.ui.principal.ui.home.Fragments.Todos;
 import com.google.android.material.tabs.TabItem;
 import com.google.android.material.tabs.TabLayout;
 
@@ -25,18 +27,44 @@ public class HomeFragment extends Fragment {
     private TabLayout mTablayout;
     private TabItem mTodos, mDulces, mComida, mFritura;
     private ViewPager mPager;
-    private PagerAdapter mAdapter;
+    PagerController pagerAdapter;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         mTablayout = (TabLayout) root.findViewById(R.id.tablayoutR);
+        mPager = (ViewPager) root.findViewById(R.id.viewpager);
 
-        mTablayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+        mTodos = (TabItem) root.findViewById(R.id.todos);
+        mDulces = (TabItem) root.findViewById(R.id.dulces);
+        mComida = (TabItem) root.findViewById(R.id.comida);
+        mFritura = (TabItem) root.findViewById(R.id.fritura);
+
+        pagerAdapter = new PagerController(getFragmentManager(), mTablayout.getTabCount());
+        mPager.setAdapter(pagerAdapter);
+
+        mTablayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
+                mPager.setCurrentItem(tab.getPosition());
 
+                if(tab.getPosition()==0){
+                    pagerAdapter.notifyDataSetChanged();
+                    Toast.makeText(getContext(), "Todos", Toast.LENGTH_LONG).show();
+                }
+                if(tab.getPosition()==1){
+                    //pagerAdapter.notifyDataSetChanged();
+                    Toast.makeText(getContext(), "Dulces", Toast.LENGTH_LONG).show();
+                }
+                if(tab.getPosition()==2){
+                    //pagerAdapter.notifyDataSetChanged();
+                    Toast.makeText(getContext(), "Comida", Toast.LENGTH_LONG).show();
+                }
+                if(tab.getPosition()==3){
+                    Toast.makeText(getContext(), "Frituras", Toast.LENGTH_LONG).show();
+                    //pagerAdapter.notifyDataSetChanged();
+                }
             }
 
             @Override
@@ -49,6 +77,9 @@ public class HomeFragment extends Fragment {
 
             }
         });
+        mPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(mTablayout));
+
+
         return root;
     }
 }
