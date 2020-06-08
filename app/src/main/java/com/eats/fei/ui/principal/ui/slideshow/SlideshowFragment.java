6186434a5f70104b75f0d62e833fa7c693bd8d5a
Jhair Sedeño <nemesist39@gmail.com>
2020-06-08 16:20:10 -0500

    Apartado de mostrar producto
    Signed-off-by: Jhair Sedeño <nemesist39@gmail.com>

--- app/src/main/java/com/eats/fei/ui/principal/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/com/eats/fei/ui/principal/ui/slideshow/SlideshowFragment.java
@@ -13,22 +13,39 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.eats.fei.R;
 import com.eats.fei.ui.registrar.RegistrarActivity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SlideshowFragment extends Fragment {
+
+    private RecyclerView recyclerViewProducto;
+    private RecyclerViewAdaptador adaptadorProducto;
 
     private SlideshowViewModel slideshowViewModel;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        slideshowViewModel =
-                ViewModelProviders.of(this).get(SlideshowViewModel.class);
+    public View onCreateView(@NonNull LayoutInflater inflater,ViewGroup container, Bundle savedInstanceState) {
+        slideshowViewModel = ViewModelProviders.of(this).get(SlideshowViewModel.class);
         View root = inflater.inflate(R.layout.fragment_slideshow, container, false);
 
-        final Button registarP = root.findViewById(R.id.button3);
+        //Rellenar productos
+        recyclerViewProducto = (RecyclerView)root.findViewById(R.id.recyclerProducto);
+        recyclerViewProducto.setLayoutManager(new LinearLayoutManager(SlideshowFragment.this.getContext()));
 
+        adaptadorProducto = new RecyclerViewAdaptador(obtenerProducto());
+        recyclerViewProducto.setAdapter(adaptadorProducto);
+
+
+
+
+        //Boton agregar producto
+        final Button registarP = root.findViewById(R.id.button3);
+        //Actividad para registrar producto
         registarP.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -36,5 +53,15 @@ public class SlideshowFragment extends Fragment {
             }
         });
         return root;
+    }
+
+    public List<ProductoModelo> obtenerProducto(){
+        List<ProductoModelo> producto = new ArrayList<>();
+        producto.add(new ProductoModelo("Dulce 1","$10.00","Dulce muy rico",R.drawable.dulce));
+        producto.add(new ProductoModelo("Dulce 2","$5.00","Dulce muy rico",R.drawable.dulce));
+        producto.add(new ProductoModelo("Dulce 3","$7.00","Dulce muy rico",R.drawable.dulce));
+        producto.add(new ProductoModelo("Dulce 4","$9.00","Dulce muy rico",R.drawable.dulce));
+
+        return producto;
     }
 }
